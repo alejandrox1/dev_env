2898832d8422ce850b39db86534bb3a24e6ecc7a
alejandrox1 <alarcj137@gmail.com>
2017-10-17 11:06:38 -0500

    go install: create gopath dir

--- go_install.sh
+++ go_install.sh
@@ -4,6 +4,6 @@
 sudo add-apt-repository ppa:gophers/archive 
 sudo apt update -y && sudo apt-get install -y golang-1.8-go
 
-
+mkdir -p $HOME/go
 echo 'export PATH=/usr/lib/go-1.8/bin:$PATH' >> ~/.bashrc                                   
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
