e3082c35ebc6d861039e8a5e0cce56861bc2f243
alejandrox1 <alarcj137@gmail.com>
2017-12-25 13:29:42 -0500

    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO
+
+# System control
+* Exec management by user/group
+
+## Go CLIs
+* Add DNS server 
+* Find LAN connected devices 
+* Workspace synchronization
--- languages/go_install.sh
+++ languages/go_install.sh
@@ -2,8 +2,10 @@
 
 
 sudo add-apt-repository ppa:gophers/archive 
-sudo apt update -y && sudo apt-get install -y golang-1.8-go
+sudo apt update -y && sudo apt-get install -y golang-1.9-go
 
 mkdir -p $HOME/go
-echo 'export PATH=/usr/lib/go-1.8/bin:$PATH' >> ~/.bashrc                                   
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export GOROOT=/usr/lib/go-1.9/' >> ~/.bashrc                                                  
+echo 'export PATH=$PATH:${GOROOT}/bin' >> ~/.bashrc                                    
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc                                   
+echo 'export PATH=${GOPATH}/bin:${PATH}' >> ~/.bashrc
