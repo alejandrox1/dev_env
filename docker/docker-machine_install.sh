7a4fb0550e32cd9d7d20ebb21475291dae75f2a7
alejandrox1 <alarcj137@gmail.com>
2018-03-17 14:15:31 -0400

    changes:docker/docker-machine_install.sh install virtualbox + docker-machine

--- docker/docker-machine_install.sh
+++ docker/docker-machine_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo apt-get update -y && sudo apt-get install -y virtualbox 
+
 curl -L https://github.com/docker/machine/releases/download/v0.14.0/docker-machine-`uname -s`-`uname -m` > docker-machine-copy && \
     sudo install docker-machine-copy /usr/local/bin/docker-machine && rm docker-machine-copy
