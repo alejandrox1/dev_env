bc280fc1bf2f78b2737947bd600d1cfd69517c8b
alejandrox1 <alarcj137@gmail.com>
2018-05-11 14:46:26 -0400

    nvidia/cri.sh Register nvidia container runtime with docker

--- nvidia/cri.sh
+++ nvidia/cri.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #
-# Install the nvidia container runtime.
+# Install the nvidia container runtime
+#   https://github.com/NVIDIA/nvidia-container-runtime
 #
 # To get the appropiate repository for a given distro see:
 #   https://nvidia.github.io/nvidia-container-runtime/
 #
 set -e
 
+# Install the container runtime.
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
     sudo apt-key add -
 
@@ -16,3 +18,21 @@ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidi
 
 sudo apt-get update -y && \
     sudo apt-get install -y nvidia-container-runtime
+
+# Keeping this two commands here to manage the docker daemon in case we want to
+# test out rm/adding this runtime.
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker
+
+# Daemon configuration file
+sudo tee /etc/docker/daemon.json <<EOF
+{
+    "runtimes": {
+        "nvidia": {
+            "path": "/usr/bin/nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOF
+sudo pkill -SIGHUP dockerd
