82a97b00989e38ab776029d4781aabfde11880ce
alejandrox1 <alarcj137@gmail.com>
2018-06-18 10:36:26 -0500

    languages/go_install.sh fixed $PATH related errors

--- languages/go_install.sh
+++ languages/go_install.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:gophers/archive
 sudo apt update -y && sudo apt-get install -y golang-${GOV}-go
 
 mkdir -p $HOME/go
-echo "export GOROOT=/usr/lib/go-${GOV}/" >> ~/.bashrc                                                  
+echo "export GOROOT=/usr/lib/go-${GOV}" >> ~/.bashrc                                                  
 echo 'export PATH=$PATH:${GOROOT}/bin' >> ~/.bashrc                                    
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc                                   
-echo 'export PATH=${GOPATH}/bin:${PATH}' >> ~/.bashrc
+echo 'export PATH=${PATH}:${GOPATH}/bin' >> ~/.bashrc
