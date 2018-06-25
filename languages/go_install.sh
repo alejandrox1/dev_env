e85b7599a9c58dda518a7de736978d76fb23155b
alejandrox1 <alarcj137@gmail.com>
2018-06-25 14:46:01 -0500

    languages/go_install.sh correct order for searching through $PATH
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- languages/go_install.sh
+++ languages/go_install.sh
@@ -7,6 +7,6 @@ sudo apt update -y && sudo apt-get install -y golang-${GOV}-go
 
 mkdir -p $HOME/go
 echo "export GOROOT=/usr/lib/go-${GOV}" >> ~/.bashrc                                                  
-echo 'export PATH=$PATH:${GOROOT}/bin' >> ~/.bashrc                                    
+echo 'export PATH=${GOROOT}/bin:$PATH' >> ~/.bashrc                                    
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc                                   
-echo 'export PATH=${PATH}:${GOPATH}/bin' >> ~/.bashrc
+echo 'export PATH=${GOPATH}/bin:$PATH' >> ~/.bashrc
