ebd2bd991a16895a48333dc5412489a23d42fd6c
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-12-07 14:07:42 -0600

    Update go install script
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

--- languages/go_install.sh
+++ languages/go_install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-GOV="1.10"
+GOV="1.11.2"
 
-sudo add-apt-repository ppa:gophers/archive 
-sudo apt update -y && sudo apt-get install -y golang-${GOV}-go
+cd /tmp
+
+wget https://dl.google.com/go/go${GOV}.linux-amd64.tar.gz
+
+sudo tar -C /usr/local -xzf go${GOV}.linux-amd64.tar.gz
 
 mkdir -p $HOME/go
-echo "export GOROOT=/usr/lib/go-${GOV}" >> ~/.bashrc                                                  
-echo 'export PATH=${GOROOT}/bin:$PATH' >> ~/.bashrc                                    
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc                                   
-echo 'export PATH=${GOPATH}/bin:$PATH' >> ~/.bashrc
+
+echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc 
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc 
