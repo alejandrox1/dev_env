a9a25248eb18376de9c3bcbf5df60e993ad5ba54
Jorge Alarcon Ochoa <jorge.alarcon@vividcortex.com>
2019-01-07 11:21:09 -0600

    Updated go install scripts
    Signed-off-by: Jorge Alarcon Ochoa <jorge.alarcon@vividcortex.com>

--- languages/go_mac_install.sh
+++ languages/go_mac_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GOV="1.11.4"
-PKG="go${GOV}.linux-amd64.tar.gz"
+PKG="go${GOV}.darwin-amd64.tar.gz"
 
 cd /tmp
 
@@ -11,10 +11,10 @@ sudo tar -C /usr/local -xzf ${PKG}
 
 mkdir -p $HOME/go
 
-cat << EOF >> ~/.bashrc
+cat << EOF >> $HOME/.bash_profile
 
 # Go settings.
 export GOROOT=/usr/local/go
 export GOPATH=\$HOME/go 
-export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH
+export PATH=\$GOPATH/bin:\$GOROOT/bin:\$PATH 
 EOF
